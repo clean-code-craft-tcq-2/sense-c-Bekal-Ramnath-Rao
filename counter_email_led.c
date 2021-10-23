2a1584f41e1942c438ed3b1d67a93caa33f2ff2e
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2021-10-23 12:08:54 +0530

    files added for email and led alert

---
+++ counter_email_led.c
@@ -0,0 +1,35 @@
+#include "counter_email_led.h"
+#include "stdio.h"
+
+/*global variable defined which is set when max is greater than threshold which is used to send an email*/
+int emailAlertCallCount = 0;
+
+/*global variable defined which is set when max is greater than threshold which is used to glow led*/
+int ledAlertCallCount = 0;
+
+/*function defined to set emailAlertCallCount when max is greater than threshold which in turn sends an email*/
+void emailAlerter()
+{
+	emailAlertCallCount = 1;
+}
+
+/*function defined to set ledAlertCallCount when max is greater than threshold which in turn glows led*/
+void ledAlerter()
+{
+	ledAlertCallCount = 1;
+}
+
+/*function defined to check whether max is greater than threshold and also sends email and glows led*/
+void check_and_alert(float Threshold, alerter_funcptr alerters[], struct Stats computedStats)
+{ 
+	if(computedStats.max > Threshold)
+	{
+		(*alerters[0])();
+		(*alerters[1])();
+	}
+}
+
+int main()
+{
+	return 0;
+}
