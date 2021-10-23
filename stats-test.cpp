3ee6d7307ac8e01b6c3499b907496465e23ef32d
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2021-10-23 12:37:37 +0530

    Changed from NAN to nan,just to check

--- stats-test.cpp
+++ stats-test.cpp
@@ -21,7 +21,7 @@ TEST_CASE("average is NaN for empty array") {
     Stats computedStats = compute_statistics(0, 0);
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
-    REQUIRE(computedStats.average == NAN);
+    REQUIRE(computedStats.average == nan);
     REQUIRE(computedStats.max == NAN);
     REQUIRE(computedStats.min == NAN);
 
