f2764b87612c7f19a79378dfd320d78a4ccb9871
Bekal Ramnath Rao <37608322+Bekal-Ramnath-Rao@users.noreply.github.com>
2021-10-23 19:46:02 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -48,7 +48,7 @@ TEST_CASE("raises alerts when max is greater than threshold") {
 
     float numberset[] = {99.8, 34.2, 4.5};
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
-    Stats computedStats = compute_statistics(numberset, setlength);
+    computedStats = compute_statistics(numberset, setlength);
 
     float maxThreshold = 10.2;
     check_and_alert(maxThreshold, alerters, computedStats);
